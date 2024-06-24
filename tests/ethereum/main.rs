653d81759d283ac77c1b94ac51f67e0e4d52f982
jackwener <jakevingoo@gmail.com>
2024-06-24 20:37:08 +0800

    chore: remove done_marker

--- tests/ethereum/main.rs
+++ tests/ethereum/main.rs
@@ -103,7 +103,7 @@ fn run_test_unit(path: &Path, unit: &TestUnit) {
     unit.post.par_iter().for_each(|(spec_name, tests)| {
         // Constantinople was immediately extended by Petersburg.
         // There was technically never a Constantinople transaction on mainnet
-        // so REVM undestandably doesn't support it (without Petersburg).
+        // so REVM understandably doesn't support it (without Petersburg).
         if *spec_name == SpecName::Constantinople {
             return;
         }
